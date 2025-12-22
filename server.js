6db84e5f00b75778a4ca7c204f50fd541075576d
ksuibraheem <ksuibraheem@gmail.com>
2025-12-22 15:08:00 +0300

    Fix Express 5 wildcard route compatibility

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA routing - send all requests to index.html
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
